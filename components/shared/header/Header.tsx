b183ef6d7ddb9a1a8fd770fee6609eb0560a29a0
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-05-03 14:05:28 +0600

    search input, wishlist, cart & user's links added

--- components/shared/header/Header.tsx
+++ components/shared/header/Header.tsx
@@ -1,14 +1,20 @@
-import Image from "next/image";
-import Link from "next/link";
 import Logo from "./Logo";
 import Search from "./Search";
+import Cart from "./Cart";
+import UserSetting from "./UserSettings";
+import { Separator } from "@/components/ui/separator";
 
 export default function Header() {
   return (
     <header className=" bg-[#053379]">
       <div className="main-container py-3 2xl:py-8 flex-between gap-8">
-        <Logo />
+        <Logo logoSrc="/assets/icons/pixel-cart-logo.svg" />
         <Search />
+        <div className="flex-between gap-12">
+          <Cart />
+          <Separator orientation="vertical" className="h-16 bg-gray-500" />
+          <UserSetting />
+        </div>
       </div>
     </header>
   );
--- components/shared/header/Search.tsx
+++ components/shared/header/Search.tsx
@@ -1,5 +1,28 @@
+import { CiSearch } from "react-icons/ci";
+
 const Search = () => {
-  return <div className="text-white"></div>;
+  return (
+    <div className="text-white">
+      <label
+        htmlFor="search"
+        className="flex-center rounded-full bg-white px-4 py-3"
+      >
+        <CiSearch
+          size={48}
+          className="2xl:w-8 mr-2 cursor-pointer"
+          color="#053379"
+        />
+
+        <input
+          type="search"
+          name=""
+          id=""
+          className="px-4 py-3 text-primaryText 2xl:text-lg 2xl:min-w-[500px] border-none outline-none 2xl:placeholder:text-lg"
+          placeholder="Enter your keywords..."
+        />
+      </label>
+    </div>
+  );
 };
 
 export default Search;
