c3290e426e4beaa7f09ee5f3352c8b128261400e
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-07-03 12:45:47 +0600

    [ADD] Browsing collection menu's static data added

--- components/shared/header/ShoppingOptionsNavigation.tsx
+++ components/shared/header/ShoppingOptionsNavigation.tsx
@@ -5,7 +5,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
-import { browsingLinks } from "@/constants";
+import { browsingLinks } from "@/data";
 import Image from "next/image";
 import Link from "next/link";
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
@@ -22,7 +22,7 @@ const ShoppingOptionsNavigation: React.FC<IShoppingOptionsNavigation> = ({
   subMenuWidth,
 }) => {
   // STATE //
-
+  const [hoverMenu, setHoverMenu] = useState<boolean>(false);
   // for active sub menu
   const [activeSubMenu, setActiveSubMenu] = useState<number>(0);
 
@@ -39,106 +39,135 @@ const ShoppingOptionsNavigation: React.FC<IShoppingOptionsNavigation> = ({
   };
 
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        {/* TODO: make home item + product item */}
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="text-primaryText hover:text-primaryGreen h-12 xl:h-16 px-6 flex items-center gap-4">
-            <HiOutlineMenuAlt2
-              size={20}
-              className="w-4 xl:w-5 group-hover:text-primaryGreen transition-all duration-200"
-            />
-            <span className="relative -right-1 2xl:-right-2 uppercase font-semibold text-[10px] xl:text-sm">
-              Browse all collection
-            </span>
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className="w-full">
-            {/* content should have a fixed width */}
-            <div
-              onMouseLeave={handleDefaultActiveMenu}
-              className="min-h-[380px] flex gap-4 xl:gap-7 bg-white"
-              style={{ width: `${subMenuWidth}px` }}
+    <div className="flex items-center gap-4">
+      <NavigationMenu>
+        <NavigationMenuList>
+          {/* TODO: make home item + product item */}
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              onMouseEnter={() => setHoverMenu(true)}
+              onMouseLeave={() => setHoverMenu(false)}
+              className="text-primaryText hover:text-primaryGreen h-12 xl:h-16 px-6 flex items-center gap-4"
             >
-              {/* menus */}
-              <div className="border-r border-gray-200 px-3 py-4 relative">
-                {/* animated div element */}
-                <motion.div
-                  variants={widthVariant}
-                  initial="initial"
-                  animate="enter"
-                  className="h-[2px] bg-accentLightLime absolute left-0 top-0"
-                />
-                {/* content */}
-                <ul className="grid grid-cols-1 gap-2">
-                  {browsingLinks &&
-                    browsingLinks.map((link, idx) => (
-                      <button
-                        key={idx}
-                        onClick={() => handleActiveMenu(idx)}
-                        onMouseEnter={() => handleActiveMenu(idx)}
-                        className="w-[220px] bg-white"
-                      >
-                        <li
-                          className={`flex items-center justify-between gap-5 px-4 py-3 bg-white ${
-                            activeSubMenu === idx && "rounded-full shadow-md"
-                          }`}
+              <HiOutlineMenuAlt2
+                size={20}
+                className={`w-4 xl:w-5 transition-all duration-200 ${
+                  hoverMenu ? "text-primaryGreen" : "text-primaryText"
+                }`}
+              />
+              <span className="relative -right-1 2xl:-right-2 uppercase font-semibold text-[10px] xl:text-sm">
+                Browse all collection
+              </span>
+            </NavigationMenuTrigger>
+            <NavigationMenuContent className="w-full">
+              {/* content should have a fixed width */}
+              <div
+                onMouseLeave={handleDefaultActiveMenu}
+                className="min-h-[380px] flex gap-4 xl:gap-7 bg-white"
+                style={{ width: `${subMenuWidth}px` }}
+              >
+                {/* menus */}
+                <div className="border-r border-gray-200 px-3 py-4 relative">
+                  {/* animated div element */}
+                  <motion.div
+                    variants={widthVariant}
+                    initial="initial"
+                    animate="enter"
+                    className="h-[2px] bg-accentLightLime absolute left-0 top-0"
+                  />
+                  {/* content */}
+                  <ul className="grid grid-cols-1 gap-2">
+                    {browsingLinks &&
+                      browsingLinks.map((link, idx) => (
+                        <button
+                          key={idx}
+                          onClick={() => handleActiveMenu(idx)}
+                          onMouseEnter={() => handleActiveMenu(idx)}
+                          className="w-[240px] bg-white"
                         >
-                          <p
-                            className={`custom-transition font-semibold ${
-                              activeSubMenu === idx && "text-primaryGreen "
+                          <li
+                            className={`flex items-center justify-between gap-5 px-4 py-3 bg-white ${
+                              activeSubMenu === idx && "rounded-full shadow-md"
                             }`}
                           >
-                            {link.label}
-                          </p>
-                          <MdKeyboardArrowRight
-                            size={20}
-                            className={`custom-transition ${
-                              activeSubMenu === idx
-                                ? "text-primaryGreen "
-                                : "text-primaryText "
-                            }`}
-                          />
+                            <p
+                              className={`custom-transition font-semibold ${
+                                activeSubMenu === idx && "text-primaryGreen "
+                              }`}
+                            >
+                              {link.label}
+                            </p>
+                            <MdKeyboardArrowRight
+                              size={20}
+                              className={`custom-transition ${
+                                activeSubMenu === idx
+                                  ? "text-primaryGreen "
+                                  : "text-primaryText "
+                              }`}
+                            />
+                          </li>
+                        </button>
+                      ))}
+                  </ul>
+                </div>
+                {/* options */}
+                <div className=" px-3 lg:px-6 pt-12 sm:pb-8 lg:pb-0 flex-1 max-h-[380px] overflow-y-auto">
+                  <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6">
+                    {browsingLinks &&
+                      browsingLinks[activeSubMenu].slug.map((slug, idx) => (
+                        <li
+                          key={idx}
+                          className="flex items-center gap-2 md:gap-3 lg:gap-5 xl:mx-4"
+                        >
+                          <div className="p-2 lg:p-3 rounded-full bg-gray-100">
+                            <Image
+                              src={slug.icon}
+                              alt={slug.label}
+                              width={40}
+                              height={40}
+                              className="w-5 h-5 lg:w-7 lg:h-7 xl:w-8 xl:h-8"
+                            />
+                          </div>
+                          <div>
+                            {/* TODO: link should be dynamic & updated */}
+                            <Link
+                              href={"#"}
+                              className="text-xs xl:text-sm font-semibold text-primaryText hover:text-primaryGreen custom-transition"
+                            >
+                              {slug.label}
+                            </Link>
+                            <p className="text-[10px] xl:text-xs text-secondaryText">
+                              From {slug.minPrice}
+                            </p>
+                          </div>
                         </li>
-                      </button>
-                    ))}
-                </ul>
+                      ))}
+                  </ul>
+                </div>
               </div>
-              {/* options */}
-              <div className=" px-6 pt-12 flex-1">
-                <ul className="grid grid-cols-3 gap-y-6">
-                  {browsingLinks &&
-                    browsingLinks[activeSubMenu].slug.map((slug, idx) => (
-                      <li key={idx} className="flex items-center gap-5 xl:mx-4">
-                        <div className="p-3 rounded-full bg-gray-100">
-                          <Image
-                            src={slug.icon}
-                            alt={slug.label}
-                            width={40}
-                            height={40}
-                            className="w-7 h-7 xl:w-8 xl:h-8"
-                          />
-                        </div>
-                        <div>
-                          {/* TODO: link should be dynamic & updated */}
-                          <Link
-                            href={"#"}
-                            className="text-xs xl:text-sm font-semibold text-primaryText hover:text-primaryGreen custom-transition"
-                          >
-                            {slug.label}
-                          </Link>
-                          <p className="text-[10px] xl:text-xs text-secondaryText">
-                            From {slug.minPrice}
-                          </p>
-                        </div>
-                      </li>
-                    ))}
-                </ul>
-              </div>
-            </div>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          {/* TODO: make home item + product item */}
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              key={1}
+              className="text-primaryText hover:text-primaryGreen h-12 xl:h-16 px-6 flex items-center gap-4"
+            >
+              <span className="relative -right-1 2xl:-right-2 uppercase font-semibold text-[10px] xl:text-sm">
+                Products
+              </span>
+            </NavigationMenuTrigger>
+            <NavigationMenuContent className="w-full">
+              {/* content should have a fixed width */}
+              <div
+                className="min-h-[280px] flex gap-4 xl:gap-7 bg-white"
+                style={{ width: `${subMenuWidth}px` }}
+              ></div>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 };
 
---
+++ data/index.ts
@@ -0,0 +1,222 @@
+export const browsingLinks = [
+  {
+    label: "Home Appliance",
+    slug: [
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/air_conditioning.svg",
+        label: "Air Conditioning",
+        minPrice: "$340",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/blender.svg",
+        label: "Blender",
+        minPrice: "$210",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/Lighting_Equipment.svg",
+        label: "Lighting Equipment",
+        minPrice: "$160",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/iron.svg",
+        label: "Flat Iron",
+        minPrice: "$80",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/Electric_Fan.svg",
+        label: "Electric Fan",
+        minPrice: "$290",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/Heating_Equipment.svg",
+        label: "Heating Equipment",
+        minPrice: "$220",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/Refrigerator.svg",
+        label: "Refrigerator",
+        minPrice: "$370",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/microwave-oven.svg",
+        label: "Microwave Oven",
+        minPrice: "$120",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/Electric_Cooker.svg",
+        label: "Electric Cooker",
+        minPrice: "$50",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/washing-machine.svg",
+        label: "Washing Machine",
+        minPrice: "$130",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/electric-stove.svg",
+        label: "Electric Stove",
+        minPrice: "$80",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/homeAppliance/vacuum.svg",
+        label: "Vacuum Cleaner",
+        minPrice: "$130",
+      },
+    ],
+  },
+  {
+    label: "Gaming Gears",
+    slug: [
+      {
+        icon: "/assets/icons/browsing-icons/gamingGears/computer-mouse.svg",
+        label: "Computer Mouse",
+        minPrice: "$30",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/gamingGears/display.svg",
+        label: "Display",
+        minPrice: "$100",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/gamingGears/Game_Pad.svg",
+        label: "Game Pad",
+        minPrice: "$30",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/gamingGears/Gaming_Chair.svg",
+        label: "Gaming Chair",
+        minPrice: "$260",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/gamingGears/keyboard.svg",
+        label: "Keyboard",
+        minPrice: "$20",
+      },
+    ],
+  },
+  {
+    label: "Computers & Laptop",
+    slug: [
+      {
+        icon: "/assets/icons/browsing-icons/computers/cooler-fan.svg",
+        label: "Cooler Fan",
+        minPrice: "$40",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/computers/cpu.svg",
+        label: "Cpu",
+        minPrice: "$120",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/computers/keyboard.svg",
+        label: "Keyboard",
+        minPrice: "$44",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/computers/laptop.svg",
+        label: "Laptop",
+        minPrice: "$570",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/computers/monitor.svg",
+        label: "Monitor",
+        minPrice: "$120",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/computers/mouse.svg",
+        label: "Mouse",
+        minPrice: "$30",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/computers/gpu.svg",
+        label: "Gpu",
+        minPrice: "$240",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/computers/workstation.svg",
+        label: "Workstation",
+        minPrice: "$450",
+      },
+    ],
+  },
+  {
+    label: "Smartphone & Tablet",
+    slug: [
+      {
+        icon: "/assets/icons/browsing-icons/phone/apple.svg",
+        label: "Apple",
+        minPrice: "$870",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/phone/tablet.svg",
+        label: "Tablet",
+        minPrice: "$330",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/phone/huawei.svg",
+        label: "Huawei",
+        minPrice: "$740",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/phone/phone.svg",
+        label: "Phone",
+        minPrice: "$180",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/phone/xiaomi.svg",
+        label: "Xiaomi",
+        minPrice: "$120",
+      },
+    ],
+  },
+  {
+    label: "Audio Gears",
+    slug: [
+      {
+        icon: "/assets/icons/browsing-icons/audio/wireless.svg",
+        label: "True Wireless",
+        minPrice: "$90",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/audio/headphones.svg",
+        label: "Headphones",
+        minPrice: "$280",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/audio/speaker.svg",
+        label: "Speaker",
+        minPrice: "$350",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/audio/earbud.svg",
+        label: "Earbud",
+        minPrice: "$120",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/audio/computer-speaker.svg",
+        label: "Computer Speaker",
+        minPrice: "$240",
+      },
+    ],
+  },
+  {
+    label: "Cameras",
+    slug: [
+      {
+        icon: "/assets/icons/browsing-icons/camera/cctv-camera.svg",
+        label: "Cctv Camera",
+        minPrice: "$490",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/camera/webcam.svg",
+        label: "Webcam",
+        minPrice: "$230",
+      },
+      {
+        icon: "/assets/icons/browsing-icons/camera/gopro.svg",
+        label: "Gopro",
+        minPrice: "$140",
+      },
+    ],
+  },
+];
